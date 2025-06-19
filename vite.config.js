aea16a2504a7572ad67aa681a8e592b4930a0e47
ElDav1d <dvivoes@yahoo.es>
2025-06-19 10:54:23 +0200

    migrate to react plus ts

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,19 @@
-import tailwindcss from '@tailwindcss/vite';
-import { defineConfig } from 'vite';
-import { svelte } from '@sveltejs/vite-plugin-svelte';
-import path from 'path';
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [tailwindcss(), svelte()],
-	resolve: {
-		alias: {
-			$components: path.resolve('./src/components'),
-			$lib: path.resolve('./src/lib'),
-			$shadcn: path.resolve('./src/shadcn')
-		}
-	}
+  plugins: [react(), tailwindcss()],
+  server: {
+    port: 3000,
+  },
+  resolve: {
+    alias: {
+      $components: path.resolve("./src/components"),
+      $lib: path.resolve("./src/lib"),
+      $shadcn: path.resolve("./src/shadcn"),
+    },
+  },
 });

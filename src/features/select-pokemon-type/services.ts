6169589619b01a19cf58b847efc794230813402b
ElDav1d <dv>
2025-07-03 17:46:28 +0200

    cast service

--- src/features/select-pokemon-type/services.ts
+++ src/features/select-pokemon-type/services.ts
@@ -1,13 +1,14 @@
 import { url } from "../../lib/constants";
+import { IPokemonTypeItem } from "../../shared/entities";
 
-const fetchPokemonTypes = async () => {
+const fetchPokemonTypes = async (): Promise<IPokemonTypeItem[]> => {
   const response = await fetch(`${url.BASE}${url.TYPE}`);
 
   if (!response.ok) throw new Error("Failed to fetch types");
 
   const data = await response.json();
 
-  return data.results ?? [];
+  return (data.results ?? []) as IPokemonTypeItem[];
 };
 
 export const services = {

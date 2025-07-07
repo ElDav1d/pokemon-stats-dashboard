6494e8279e8344ee9afb2b4831b9da2384a3ba58
ElDav1d <dv>
2025-07-07 18:29:02 +0200

    fix smoke test

--- src/features/pokemon-list/domain/smokeTest.ts
+++ src/features/pokemon-list/domain/smokeTest.ts
@@ -1,45 +1,59 @@
-// smokeTest PokemonRepository
-
 import { url } from "../../../lib/constants.ts";
-import { Pokemon } from "./entities/Pokemon.ts";
 import type { PokemonRepository } from "./ports/PokemonRepository.ts";
 import { PokemonType } from "./value-objects/PokemonType.ts";
+import { PokemonByType } from "./value-objects/PokemonByType.ts";
+import { PokemonByName } from "./value-objects/PokemonByName.ts";
 
-export class SmokeTestPokemonRepository implements PokemonRepository {
-  private readonly pokemonList: Pokemon[] = [
-    new Pokemon(
-      "charmander",
-      `${url.BASE}${url.POKEMON}4/`,
-      6,
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png"
-    ),
-    new Pokemon(
-      "squirtle",
-      `${url.BASE}${url.POKEMON}7/`,
-      5,
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/7.png"
-    ),
-    new Pokemon(
-      "bulbasaur",
-      `${url.BASE}${url.POKEMON}1/`,
-      7,
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png"
-    ),
+class SmokeTestPokemonRepository implements PokemonRepository {
+  private readonly pokemonList: PokemonByType[] = [
+    new PokemonByType("charmander", `${url.BASE}${url.POKEMON}4/`),
+    new PokemonByType("squirtle", `${url.BASE}${url.POKEMON}7/`),
+    new PokemonByType("bulbasaur", `${url.BASE}${url.POKEMON}1/`),
   ];
 
-  async findByType(type: PokemonType): Promise<Pokemon[]> {
+  async findAllByType(type: PokemonType): Promise<PokemonByType[]> {
     console.log(`SMOKE TEST Finding Pokémon by type: ${type.value}`);
-    return this.pokemonList.filter((pokemon) =>
-      pokemon.name.includes(type.value)
-    );
+    return this.pokemonList;
+  }
+
+  async findDetailsByName(name: string): Promise<PokemonByName | null> {
+    // Fake details for each Pokémon
+    const details: Record<string, PokemonByName> = {
+      charmander: new PokemonByName(
+        6,
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png"
+      ),
+      squirtle: new PokemonByName(
+        5,
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/7.png"
+      ),
+      bulbasaur: new PokemonByName(
+        7,
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png"
+      ),
+    };
+    return details[name] ?? null;
   }
 }
 
 const smokeTestPokemonRepository = new SmokeTestPokemonRepository();
+
 (async () => {
-  const type = new PokemonType("char");
-  const pokemons = await smokeTestPokemonRepository.findByType(type);
-  console.log("Smoke Test Pokémon List:", pokemons);
+  const type = new PokemonType("test-type");
+  const listByType = await smokeTestPokemonRepository.findAllByType(type);
+  const listOfDetails = await Promise.all(
+    listByType.map((p) => smokeTestPokemonRepository.findDetailsByName(p.name))
+  );
+
+  listByType.forEach((pokemon, index) => {
+    const details = listOfDetails[index];
+    console.log(
+      `Pokemon: ${pokemon.name},
+       URL: ${pokemon.url},
+       Height: ${details?.height},
+       Image: ${details?.imageUrl}`
+    );
+  });
 })();
 
 // run npx ts-node src/features/pokemon-list/domain/smokeTest.ts

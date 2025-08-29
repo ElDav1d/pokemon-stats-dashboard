99ad29c1a8d4e3617ed31c15bd756aca3158fc74
ElDav1d <dv>
2025-08-29 14:31:58 +0200

    optimise  buttonitem render

--- src/components/select-button-list/SelectButton.tsx
+++ src/components/select-button-list/SelectButton.tsx
@@ -1,18 +1,21 @@
+import { memo } from "react";
 export interface ISelectButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   selected: boolean;
   children: React.ReactNode;
 }
 
-const SelectButton = ({ selected, children, ...props }: ISelectButtonProps) => (
-  <button
-    type="button"
-    className={selected ? "button-type-selected" : ""}
-    aria-pressed={selected}
-    {...props}
-  >
-    {children}
-  </button>
+const SelectButton = memo(
+  ({ selected, children, ...props }: ISelectButtonProps) => (
+    <button
+      type="button"
+      className={selected ? "button-type-selected" : ""}
+      aria-pressed={selected}
+      {...props}
+    >
+      {children}
+    </button>
+  )
 );
 
 export default SelectButton;

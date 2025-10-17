a6eb8ae9bd7fb04cf02ace8f19eafb33e832ac42
ElDav1d <dv>
2025-10-17 14:36:15 +0200

    red unit tests for PokemonListViewModel

---
+++ src/features/pokemon-list/application/view-models/__tests__/PokemonListViewModel.test.ts
@@ -0,0 +1,91 @@
+import { it, expect, vi } from "vitest";
+import { PokemonRepository } from "../../../domain/ports/PokemonRepository";
+import { PokemonListItem } from "../../../domain/entities/PokemonListItem";
+import { PokemonByType } from "../../../domain/value-objects/PokemonByType";
+import { PokemonListViewModel } from "../PokemonListViewModel";
+
+it("should load pokemon list by type", async () => {
+  const mockPokemon1 = new PokemonListItem(
+    "1",
+    "charizard",
+    "https://pokeapi.co/api/v2/pokemon/6/",
+    17,
+    "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/6.png"
+  );
+  const mockPokemon2 = new PokemonListItem(
+    "2",
+    "vulpix",
+    "https://pokeapi.co/api/v2/pokemon/37/",
+    6,
+    "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/37.png"
+  );
+
+  const mockRepository: PokemonRepository = {
+    findAllByType: vi.fn().mockResolvedValue([mockPokemon1, mockPokemon2]),
+    findDetailsByName: vi.fn(),
+  };
+
+  const viewModel = new PokemonListViewModel(mockRepository);
+
+  const result = await viewModel.loadPokemonList("fire");
+
+  expect(result).toHaveLength(2);
+  expect(result[0].name).toBe("charizard");
+  expect(result[1].name).toBe("vulpix");
+  expect(mockRepository.findAllByType).toHaveBeenCalledWith(
+    new PokemonByType("fire", "")
+  );
+});
+
+it("should return empty array when type is empty", async () => {
+  const mockRepository: PokemonRepository = {
+    findAllByType: vi.fn(),
+    findDetailsByName: vi.fn(),
+  };
+
+  const viewModel = new PokemonListViewModel(mockRepository);
+
+  const result = await viewModel.loadPokemonList("");
+
+  expect(result).toEqual([]);
+  expect(mockRepository.findAllByType).not.toHaveBeenCalled();
+});
+
+it("should sort pokemon list by height", () => {
+  const mockRepository: PokemonRepository = {
+    findAllByType: vi.fn(),
+    findDetailsByName: vi.fn(),
+  };
+
+  const unsortedList = [
+    new PokemonListItem(
+      "1",
+      "charizard",
+      "https://pokeapi.co/api/v2/pokemon/6/",
+      20,
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/6.png"
+    ),
+    new PokemonListItem(
+      "2",
+      "vulpix",
+      "https://pokeapi.co/api/v2/pokemon/37/",
+      6,
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/37.png"
+    ),
+    new PokemonListItem(
+      "3",
+      "charmander",
+      "https://pokeapi.co/api/v2/pokemon/4/",
+      5,
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png"
+    ),
+  ];
+
+  const viewModel = new PokemonListViewModel(mockRepository);
+
+  const result = viewModel.sortPokemonListByHeight(unsortedList);
+
+  expect(result[0].height).toBe(5);
+  expect(result[1].height).toBe(6);
+  expect(result[2].height).toBe(20);
+});

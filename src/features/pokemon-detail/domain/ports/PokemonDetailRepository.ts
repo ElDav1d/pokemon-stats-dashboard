e679e25d18b459b879d0b3096f19e30389282918
ElDav1d <dv>
2025-12-27 11:10:00 +0100

    create PokemonDetailRepository port

---
+++ src/features/pokemon-detail/domain/ports/PokemonDetailRepository.ts
@@ -0,0 +1,25 @@
+import { PokemonDetail } from "../entities/PokemonDetail";
+import { EvolutionChain } from "../entities/EvolutionChain";
+import { PokemonByType } from "../value-objects/PokemonByType";
+
+export interface PokemonDetailRepository {
+  /**
+   * Gets pokemon details by name
+   */
+  findByName(name: string): Promise<PokemonDetail>;
+
+  /**
+   * Gets the evolution chain URL from the species URL
+   */
+  findEvolutionChainUrl(speciesUrl: string): Promise<string>;
+
+  /**
+   * Gets the evolution chain from its URL
+   */
+  findEvolutionChain(evolutionChainUrl: string): Promise<EvolutionChain>;
+
+  /**
+   * Gets all pokemon of a specific type
+   */
+  findAllByType(typeName: string): Promise<PokemonByType[]>;
+}
